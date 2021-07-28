19cdd6e35eb28e81b69847f28fc93ac5e27ad5a6
shikha <kanakyadav227@gmail.com>
2021-07-28 21:40:46 +0530

    Allowed Host

--- todoList/settings.py
+++ todoList/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@x#adk&1acai_8t(tfa79d$qmgh19my*7)dgdtzgs&$!^2_fda'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['todo-227.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','todo-227.herokuapp.com']
 
 
 # Application definition
